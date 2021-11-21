acf911f3407dfb7714b1e08fbcdea3c8e99026c3
Sebastião Fortes <sebastiaofortes4@gmail.com>
2021-11-21 14:03:50 -0300

    subindo projeto para o github

--- src/main/java/com/sebastiaofortes/security/security/Controller/MainController.java
+++ src/main/java/com/sebastiaofortes/security/security/Controller/MainController.java
@@ -1,14 +1,28 @@
 package com.sebastiaofortes.security.security.Controller;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.sebastiaofortes.security.security.Rsa;
 import com.sebastiaofortes.security.security.Usuariorepository;
 import com.sebastiaofortes.security.security.Model.Usuarios;
 
@@ -20,6 +34,9 @@ public class MainController {
 	
 	@Autowired
 	private Usuariorepository userRepository;
+	
+	@Autowired
+	private ServletContext servletContext;
 
 	  @GetMapping(path="/add") // Map ONLY POST Requests
 	  public @ResponseBody String addNewUser1 (@RequestParam String name
@@ -28,8 +45,8 @@ public class MainController {
 	    // @RequestParam means it is a parameter from the GET or POST request
 
 	    Usuarios n = new Usuarios();
-	    n.setNome(name);
-	    n.setEmail(email);
+	    n.setLogin(name);
+	    n.setEmail(new BCryptPasswordEncoder().encode(email));
 	    userRepository.save(n);
 	    return "Dados salvos com sucesso!";
 	  }
@@ -41,9 +58,12 @@ public class MainController {
 	    // @RequestParam means it is a parameter from the GET or POST request
 
 	    Usuarios n = new Usuarios();
-	    n.setNome("sebastiao");
-	    n.setEmail("fortes4");
+	    n.setLogin(name);
+	    n.setEmail(new BCryptPasswordEncoder().encode(email));
 	    userRepository.save(n);
+	    
+	    Rsa chaves = new Rsa(name);
+	    chaves.start();
 	    return "Saved";
 	  }
 	
@@ -51,11 +71,33 @@ public class MainController {
 	public String index() {
 
 	    Usuarios n = new Usuarios();
-	    n.setNome("sebastiao");
+	    n.setLogin("sebastiao");
 	    n.setEmail("fortes4");
 	    userRepository.save(n);
 		
 		return "testesql";
+	}
+	
+	@RequestMapping("/listando")
+	public String listando(ModelMap model) {
+
+		List<Usuarios> lista = (List<Usuarios>) userRepository.findAll();
+		int numres = (lista.size());
+		String textres = Integer.toString(numres);
+		model.addAttribute("numeroresultados", textres);
+		model.addAttribute("usuarios", lista);
+		return "listando";
+	}
+	
+	@RequestMapping("/listando2")
+	public String listando2(ModelMap model) {
+
+		List<Usuarios> lista = (List<Usuarios>) userRepository.findAll();
+		int numres = (lista.size());
+		String textres = Integer.toString(numres);
+		model.addAttribute("numeroresultados", textres);
+		model.addAttribute("usuarios", lista);
+		return "listando2";
 	}
 	
 	@RequestMapping("/teste-sql-get")
@@ -65,11 +107,11 @@ public class MainController {
 		return "testesget";
 	}
 	
-	@RequestMapping("/teste-sql-post")
+	@RequestMapping("/cadastro")
 	public String TesteSpost() {
 
 		
-		return "testespost";
+		return "cadastro";
 	}
 	
 	@RequestMapping("/sobre")
@@ -83,4 +125,50 @@ public class MainController {
 
 		return "home";
 	}
+	
+	@RequestMapping("/exibindo")
+	public String exibindo(ModelMap model) {
+
+		   model.addAttribute("nomeDoAtributo", "Informção vinda do código java");
+		return "exibindo";
+	}
+	
+	@RequestMapping("/recebendo")
+	public String recebendo(ModelMap model, @RequestParam String valor) {
+
+		   model.addAttribute("nomeDoAtributo", valor);
+		return "recebendo";
+	}
+	
+	
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/download/{fileName}")
+    public void download(HttpServletResponse response, @PathVariable("fileName") String fileName) {
+        String path = "C:/keys/";
+        
+        Path arquivo = Paths.get(path, fileName + ".key");
+        System.out.println("Download: "+arquivo);
+
+        if(Files.exists(arquivo)) {
+           
+            response.addHeader("Content-Disposition", "attachment; filename=\"" + fileName +".key" + "\"");            
+            try {
+                Files.copy(arquivo, response.getOutputStream());
+                response.getOutputStream().flush();
+            } 
+            catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+	
+
+	@RequestMapping("/download-chaves")
+	public String dowChaves() {
+
+		return "dow-chave";
+	}
+
+
+
 }
